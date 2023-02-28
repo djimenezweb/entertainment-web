70e858c7357553b31a62ce8cc1b73d89ad6be0f8
Daniel Jiménez <djimenezweb@gmail.com>
2023-02-28 18:53:50 +0100

    styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,23 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import CatImage from '../assets/images/anubis.jpg';
+/* import CatImage from '../assets/images/anubis.jpg';
 import { sayHello } from './demo.js';
 
 sayHello();
 
 const img = document.createElement('img');
 img.src = CatImage;
-document.body.append(img);
+document.body.append(img); */
+
+/* API: https://www.themoviedb.org/ 
+Clave de la API (v3 auth)
+8c249596e745671ad9c2727c7f926d41
+
+Ejemplo de solicitud de API
+https://api.themoviedb.org/3/movie/550?api_key=8c249596e745671ad9c2727c7f926d41
+
+Imágenes
+https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
+
+Documentación: https://developers.themoviedb.org/3/getting-started/images
+*/
